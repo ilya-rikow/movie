726586521f404d9bac54e4469cdc256d8ecfd0bd
ilya-rikow <62751670+ilya-rikow@users.noreply.github.com>
2020-07-07 17:36:56 +0300

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,58 @@
 const slider = document.querySelector('.swiper-container');
 const sliderCard = document.querySelector('.slider-container');
 
-let mySwiper = new Swiper(slider, {
-	slidesPerView: 3,
-	spaceBetween: 5,
-	loop: false,
-
+var mySwiper = new Swiper(slider, {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	slidesOffsetAfter: 40,
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
-	},
-})
+    },
+    breakpoints:{
+    	480:{
+    		slidesPerView: 2,
+    		spaceBetween: 10,
+    		slidesOffsetAfter: 0,
+    	},
+    	576:{
+    		slidesPerView: 2,
+    		spaceBetween: 10,
+    	},
+    	768:{
+    		slidesPerView: 3,
+    		spaceBetween: 20,
+    	}
+    }
+
+});
 
 
 
 
-
-/*let mySwiper1;
+var mySwiper1;
 
 function mobileSlider() {
-  if (window.innerWidth <= 600 && sliderCard.dataset.mobile == 'false') {
-    mySwiper1 = new Swiper(sliderCard, {
+  if (window.innerWidth <= 767 && sliderCard.dataset.mobile == 'false') {
+  mySwiper1 = new Swiper(sliderCard, {
       slidesPerView: 1,
-      spaceBetween: 30,
-      loop: false,
+      spaceBetween: 0,
       slideClass: 'card-item',
+      loop: false,
+      centeredSlides: true,
+       breakpoints:{
+    	321:{
+    		slidesPerView: 1,
+    		spaceBetween: 30,
+    		}
+    	}
     });
 
       sliderCard.dataset.mobile = 'true';
+
   }
 
-  if (window.innerWidth > 600) {
+  if (window.innerWidth > 767) {
     sliderCard.dataset.mobile = 'false';
     if (sliderCard.classList.contains('swiper-container-initialized')) {
       mySwiper1.destroy();
@@ -42,4 +64,4 @@ mobileSlider()
 
 window.addEventListener('resize', () => {
   mobileSlider();
-});*/
+});
